b13204cfb66e88000558429e1b2f36580c526c7d
Hossam Taha <hossamtaha998@oultook.com>
2024-05-12 05:30:01 +0300

    modification

--- tweet-trend-CICD/terraform_code/V4-EC2-With_VPC_for_each.tf
+++ tweet-trend-CICD/terraform_code/V4-EC2-With_VPC_for_each.tf
@@ -9,9 +9,9 @@ resource "aws_instance" "demo-server" {
     vpc_security_group_ids = [aws_security_group.demo-sg.id]
     subnet_id = aws_subnet.dpp-public-subnet-01.id 
     for_each = toset(["jenkins-master", "build-slave", "ansible"])
-   tags = {
-     Name = "${each.key}"
-   }
+    tags = {
+        Name = "${each.key}"
+    }
 }
 
 resource "aws_security_group" "demo-sg" {
